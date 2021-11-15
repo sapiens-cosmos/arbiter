6e88a9b895460a9032dfad1fc3f326816cd1ef2c
Thunnini <yunjh1994@gmail.com>
2021-11-15 14:41:49 +0900

    Remove "sudo" to use the docker

--- dev/proto/protogen-docker.sh
+++ dev/proto/protogen-docker.sh
@@ -4,12 +4,12 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # Build the docker image first.
-sudo docker build --tag arbiter/protogen -f "$DIR"/Dockerfile "$DIR"/../..
+docker build --tag arbiter/protogen -f "$DIR"/Dockerfile "$DIR"/../..
 
 # Make sure that previous container not exist.
-sudo docker rm --force arbiter_protogen 1>/dev/null 2>&1
+docker rm --force arbiter_protogen 1>/dev/null 2>&1
 
-sudo docker run -v "$DIR"/../..:/app --name arbiter_protogen arbiter/protogen sh dev/proto/protogen.sh
+docker run -v "$DIR"/../..:/app --name arbiter_protogen arbiter/protogen sh dev/proto/protogen.sh
 
 # Clear the container
-sudo docker rm --force arbiter_protogen 1>/dev/null 2>&1
+docker rm --force arbiter_protogen 1>/dev/null 2>&1
