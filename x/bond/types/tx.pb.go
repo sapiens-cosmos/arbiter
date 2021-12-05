d69688ff2780282b65d591b8234cf80f53ea720d
Thunnini <yunjh1994@gmail.com>
2021-12-05 20:39:26 +0900

    Add the msg for redeeming debt

--- x/bond/types/tx.pb.go
+++ x/bond/types/tx.pb.go
@@ -105,15 +105,92 @@ func (m *MsgBondInResponse) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_MsgBondInResponse proto.InternalMessageInfo
 
+// MsgRedeem represents a message to redeem the debt from bonding.
+type MsgRedeem struct {
+	Bonder string `protobuf:"bytes,1,opt,name=bonder,proto3" json:"bonder,omitempty" yaml:"bonder"`
+}
+
+func (m *MsgRedeem) Reset()         { *m = MsgRedeem{} }
+func (m *MsgRedeem) String() string { return proto.CompactTextString(m) }
+func (*MsgRedeem) ProtoMessage()    {}
+func (*MsgRedeem) Descriptor() ([]byte, []int) {
+	return fileDescriptor_fd3f9cf0496a65c0, []int{2}
+}
+func (m *MsgRedeem) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *MsgRedeem) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_MsgRedeem.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *MsgRedeem) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_MsgRedeem.Merge(m, src)
+}
+func (m *MsgRedeem) XXX_Size() int {
+	return m.Size()
+}
+func (m *MsgRedeem) XXX_DiscardUnknown() {
+	xxx_messageInfo_MsgRedeem.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_MsgRedeem proto.InternalMessageInfo
+
+// MsgRedeemResponse defines the Msg/Redeem response type.
+type MsgRedeemResponse struct {
+}
+
+func (m *MsgRedeemResponse) Reset()         { *m = MsgRedeemResponse{} }
+func (m *MsgRedeemResponse) String() string { return proto.CompactTextString(m) }
+func (*MsgRedeemResponse) ProtoMessage()    {}
+func (*MsgRedeemResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_fd3f9cf0496a65c0, []int{3}
+}
+func (m *MsgRedeemResponse) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *MsgRedeemResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_MsgRedeemResponse.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *MsgRedeemResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_MsgRedeemResponse.Merge(m, src)
+}
+func (m *MsgRedeemResponse) XXX_Size() int {
+	return m.Size()
+}
+func (m *MsgRedeemResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_MsgRedeemResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_MsgRedeemResponse proto.InternalMessageInfo
+
 func init() {
 	proto.RegisterType((*MsgBondIn)(nil), "arbiter.bond.v1beta1.MsgBondIn")
 	proto.RegisterType((*MsgBondInResponse)(nil), "arbiter.bond.v1beta1.MsgBondInResponse")
+	proto.RegisterType((*MsgRedeem)(nil), "arbiter.bond.v1beta1.MsgRedeem")
+	proto.RegisterType((*MsgRedeemResponse)(nil), "arbiter.bond.v1beta1.MsgRedeemResponse")
 }
 
 func init() { proto.RegisterFile("arbiter/bond/v1beta/tx.proto", fileDescriptor_fd3f9cf0496a65c0) }
 
 var fileDescriptor_fd3f9cf0496a65c0 = []byte{
-	// 312 bytes of a gzipped FileDescriptorProto
+	// 346 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x92, 0x49, 0x2c, 0x4a, 0xca,
 	0x2c, 0x49, 0x2d, 0xd2, 0x4f, 0xca, 0xcf, 0x4b, 0xd1, 0x2f, 0x33, 0x4c, 0x4a, 0x2d, 0x49, 0xd4,
 	0x2f, 0xa9, 0xd0, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x12, 0x81, 0xca, 0xea, 0x81, 0x64, 0xf5,
@@ -126,14 +203,16 @@ var fileDescriptor_fd3f9cf0496a65c0 = []byte{
 	0xb8, 0x8d, 0x24, 0xf5, 0x20, 0xf6, 0xe8, 0x81, 0xec, 0x81, 0x39, 0x49, 0xcf, 0x39, 0x3f, 0x33,
 	0xcf, 0x49, 0xf8, 0xc4, 0x3d, 0x79, 0x86, 0x4f, 0xf7, 0xe4, 0xb9, 0x21, 0xe6, 0x80, 0x34, 0x29,
 	0x05, 0x81, 0xf5, 0x5a, 0x71, 0x74, 0x2c, 0x90, 0x67, 0x78, 0xb1, 0x40, 0x9e, 0x41, 0x49, 0x98,
-	0x4b, 0x10, 0xee, 0x8a, 0xa0, 0xd4, 0xe2, 0x82, 0xfc, 0xbc, 0xe2, 0x54, 0xa3, 0x48, 0x2e, 0x66,
-	0xdf, 0xe2, 0x74, 0xa1, 0x20, 0x2e, 0x36, 0xa8, 0xf3, 0xe4, 0xf5, 0xb0, 0xf9, 0x5c, 0x0f, 0xae,
-	0x53, 0x4a, 0x9d, 0x80, 0x02, 0x98, 0xd1, 0x4e, 0x1e, 0x27, 0x1e, 0xc9, 0x31, 0x5e, 0x78, 0x24,
-	0xc7, 0xf8, 0xe0, 0x91, 0x1c, 0xe3, 0x84, 0xc7, 0x72, 0x0c, 0x17, 0x1e, 0xcb, 0x31, 0xdc, 0x78,
-	0x2c, 0xc7, 0x10, 0xa5, 0x97, 0x9e, 0x59, 0x92, 0x51, 0x9a, 0xa4, 0x97, 0x9c, 0x9f, 0xab, 0x5f,
-	0x9c, 0x58, 0x90, 0x99, 0x9a, 0x57, 0xac, 0x0b, 0x0d, 0x43, 0x58, 0xa4, 0x54, 0x40, 0xa2, 0xa5,
-	0xa4, 0xb2, 0x20, 0xb5, 0x38, 0x89, 0x0d, 0x1c, 0x8e, 0xc6, 0x80, 0x00, 0x00, 0x00, 0xff, 0xff,
-	0x7b, 0x6c, 0x09, 0x52, 0xb3, 0x01, 0x00, 0x00,
+	0x4b, 0x10, 0xee, 0x8a, 0xa0, 0xd4, 0xe2, 0x82, 0xfc, 0xbc, 0xe2, 0x54, 0x25, 0x07, 0xb0, 0xd3,
+	0x82, 0x52, 0x53, 0x52, 0x53, 0x73, 0x49, 0x70, 0x1a, 0x86, 0xb1, 0x10, 0x13, 0x60, 0xc6, 0x1a,
+	0xad, 0x65, 0xe4, 0x62, 0xf6, 0x2d, 0x4e, 0x17, 0x0a, 0xe2, 0x62, 0x83, 0x7a, 0x5b, 0x5e, 0x0f,
+	0x5b, 0x88, 0xea, 0xc1, 0x5d, 0x24, 0xa5, 0x4e, 0x40, 0x01, 0xcc, 0x6c, 0x90, 0x99, 0x50, 0xf7,
+	0xe2, 0x36, 0x13, 0xa2, 0x00, 0x8f, 0x99, 0xa8, 0xee, 0x75, 0xf2, 0x38, 0xf1, 0x48, 0x8e, 0xf1,
+	0xc2, 0x23, 0x39, 0xc6, 0x07, 0x8f, 0xe4, 0x18, 0x27, 0x3c, 0x96, 0x63, 0xb8, 0xf0, 0x58, 0x8e,
+	0xe1, 0xc6, 0x63, 0x39, 0x86, 0x28, 0xbd, 0xf4, 0xcc, 0x92, 0x8c, 0xd2, 0x24, 0xbd, 0xe4, 0xfc,
+	0x5c, 0xfd, 0xe2, 0xc4, 0x82, 0xcc, 0xd4, 0xbc, 0x62, 0x5d, 0x68, 0x7c, 0xc3, 0x12, 0x50, 0x05,
+	0x24, 0x09, 0x95, 0x54, 0x16, 0xa4, 0x16, 0x27, 0xb1, 0x81, 0xe3, 0xdc, 0x18, 0x10, 0x00, 0x00,
+	0xff, 0xff, 0x37, 0xaf, 0x2e, 0xb7, 0x5f, 0x02, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -150,6 +229,8 @@ const _ = grpc.SupportPackageIsVersion4
 type MsgClient interface {
 	// BondIn defines a method for bonding some amount of coin to receive the base coin.
 	BondIn(ctx context.Context, in *MsgBondIn, opts ...grpc.CallOption) (*MsgBondInResponse, error)
+	// Redeem defines a method for redeeming the debt from bonding.
+	Redeem(ctx context.Context, in *MsgRedeem, opts ...grpc.CallOption) (*MsgRedeemResponse, error)
 }
 
 type msgClient struct {
@@ -169,10 +250,21 @@ func (c *msgClient) BondIn(ctx context.Context, in *MsgBondIn, opts ...grpc.Call
 	return out, nil
 }
 
+func (c *msgClient) Redeem(ctx context.Context, in *MsgRedeem, opts ...grpc.CallOption) (*MsgRedeemResponse, error) {
+	out := new(MsgRedeemResponse)
+	err := c.cc.Invoke(ctx, "/arbiter.bond.v1beta1.Msg/Redeem", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // MsgServer is the server API for Msg service.
 type MsgServer interface {
 	// BondIn defines a method for bonding some amount of coin to receive the base coin.
 	BondIn(context.Context, *MsgBondIn) (*MsgBondInResponse, error)
+	// Redeem defines a method for redeeming the debt from bonding.
+	Redeem(context.Context, *MsgRedeem) (*MsgRedeemResponse, error)
 }
 
 // UnimplementedMsgServer can be embedded to have forward compatible implementations.
@@ -181,6 +273,9 @@ type UnimplementedMsgServer struct {
 
 func (*UnimplementedMsgServer) BondIn(ctx context.Context, req *MsgBondIn) (*MsgBondInResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method BondIn not implemented")
+}
+func (*UnimplementedMsgServer) Redeem(ctx context.Context, req *MsgRedeem) (*MsgRedeemResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Redeem not implemented")
 }
 
 func RegisterMsgServer(s grpc1.Server, srv MsgServer) {
@@ -205,6 +300,24 @@ func _Msg_BondIn_Handler(srv interface{}, ctx context.Context, dec func(interfac
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Msg_Redeem_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(MsgRedeem)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(MsgServer).Redeem(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/arbiter.bond.v1beta1.Msg/Redeem",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(MsgServer).Redeem(ctx, req.(*MsgRedeem))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Msg_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "arbiter.bond.v1beta1.Msg",
 	HandlerType: (*MsgServer)(nil),
@@ -212,6 +325,10 @@ var _Msg_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "BondIn",
 			Handler:    _Msg_BondIn_Handler,
+		},
+		{
+			MethodName: "Redeem",
+			Handler:    _Msg_Redeem_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
@@ -281,6 +398,59 @@ func (m *MsgBondInResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	return len(dAtA) - i, nil
 }
 
+func (m *MsgRedeem) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *MsgRedeem) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *MsgRedeem) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if len(m.Bonder) > 0 {
+		i -= len(m.Bonder)
+		copy(dAtA[i:], m.Bonder)
+		i = encodeVarintTx(dAtA, i, uint64(len(m.Bonder)))
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *MsgRedeemResponse) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *MsgRedeemResponse) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *MsgRedeemResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	return len(dAtA) - i, nil
+}
+
 func encodeVarintTx(dAtA []byte, offset int, v uint64) int {
 	offset -= sovTx(v)
 	base := offset
@@ -308,6 +478,28 @@ func (m *MsgBondIn) Size() (n int) {
 }
 
 func (m *MsgBondInResponse) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	return n
+}
+
+func (m *MsgRedeem) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = len(m.Bonder)
+	if l > 0 {
+		n += 1 + l + sovTx(uint64(l))
+	}
+	return n
+}
+
+func (m *MsgRedeemResponse) Size() (n int) {
 	if m == nil {
 		return 0
 	}
@@ -464,6 +656,138 @@ func (m *MsgBondInResponse) Unmarshal(dAtA []byte) error {
 		}
 		if fieldNum <= 0 {
 			return fmt.Errorf("proto: MsgBondInResponse: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		default:
+			iNdEx = preIndex
+			skippy, err := skipTx(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthTx
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *MsgRedeem) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowTx
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: MsgRedeem: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: MsgRedeem: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Bonder", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowTx
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthTx
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthTx
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Bonder = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipTx(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthTx
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *MsgRedeemResponse) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowTx
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: MsgRedeemResponse: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: MsgRedeemResponse: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
 		default:
